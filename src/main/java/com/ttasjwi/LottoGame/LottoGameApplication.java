be6cd0a681dcd484dfa705a8ec2c0ee33a54ae74
ttasjwi <ttasjwi920@gmail.com>
2022-02-23 18:01:56 +0900

    프로젝트 초기화
    - 프로젝트 생성을 위한 초기 설정파일을 추가함.
    - README.md 작성

---
+++ src/main/java/com/ttasjwi/LottoGame/LottoGameApplication.java
@@ -0,0 +1,13 @@
+package com.ttasjwi.LottoGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottoGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttasjwi/LottoGame/LottoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttasjwi.LottoGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
